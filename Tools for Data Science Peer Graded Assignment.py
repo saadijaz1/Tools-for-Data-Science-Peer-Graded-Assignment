d0776cb5a6ab5c953ad20bf3b0c219b1a8f656db
Saad Ijaz <103124988+saadijaz1@users.noreply.github.com>
2022-09-14 19:15:39 +0300

    Update Tools for Data Science Peer Graded Assignment.py

--- Tools for Data Science Peer Graded Assignment.py
+++ Tools for Data Science Peer Graded Assignment.py
@@ -1,39 +1,35 @@
-#!/usr/bin/env python
-# coding: utf-8
+!/usr/bin/env python
+coding: utf-8
 
-# <center>
-#     <img src="https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/Logos/organization_logo/organization_logo.png" width="300" alt="cognitiveclass.ai logo"  />
-# </center>
-# 
+<center>
+<img src="https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/Logos/organization_logo/organization_logo.png" width="300" alt="cognitiveclass.ai logo"  />
+</center>
 
-# # My Jupyter Notebook on IBM Watson Studio
+#My Jupyter Notebook on IBM Watson Studio
 
-# **Saad Ijaz**<br/>
-# Data Scientist
+**Saad Ijaz**<br/>
+Data Scientist
 
-# *I am intersted in Data Science because I like analyzing data and finding patterns.*
+*I am intersted in Data Science because I like analyzing data and finding patterns.*
 
-# ### My code will take value of two variable and print their sum
-
-# In[2]:
-
+### My code will take value of two variable and print their sum
 
 a = 1
 b = 2
 print(a+b)
 
 
-# This is an example of bulleted list:
-# * One
-# * Two
-# * Three
-# 
-# Following is an example of numbered list:
-# 1. Data Analyst
-# 2. Data Engineer
-# 3. Data Scientist
-# 
-# This is an example of strikethrough<br/>
-# ~~This is an example of strikethrough~~
-# 
-# This is the link to my [GitHub Page](https://github.com/saadijaz1/saadijaz1).
+This is an example of bulleted list:
+* One
+* Two
+* Three
+
+Following is an example of numbered list:
+1. Data Analyst
+2. Data Engineer
+3. Data Scientist
+
+This is an example of strikethrough<br/>
+~~This is an example of strikethrough~~
+
+This is the link to my [GitHub Page](https://github.com/saadijaz1/saadijaz1).
